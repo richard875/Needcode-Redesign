8be535c7feb5fdc8da8a0593c0dc9e8b3b649e3a
Richard Lee <richard_875@me.com>
2022-06-28 01:42:19 +1000

    - Define custom components and app metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,29 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // app
+  app: {
+    head: {
+      title: "A better way to prepare for coding interviews - Neetcode.io",
+      titleTemplate:
+        "%s - A better way to prepare for coding interviews - Neetcode.io",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        // {
+        //   hid: "description",
+        //   name: "description",
+        //   content: "A better way to prepare for coding interviews - Neetcode.io",
+        // },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "assets/images/assembly_icon.svg",
+        },
+      ],
+    },
+  },
   typescript: {
     shim: false,
   },
@@ -19,6 +42,21 @@ export default defineNuxtConfig({
           "bx-tag",
           "bx-side-nav-divider",
           "bx-side-nav-link",
+          "bx-data-table",
+          "bx-table",
+          "bx-table-head",
+          "bx-table-header-row",
+          "bx-table-header-cell",
+          "bx-table-body",
+          "bx-table-row",
+          "bx-table-cell",
+          "bx-modal",
+          "bx-modal-header",
+          "bx-modal-close-button",
+          "bx-modal-label",
+          "bx-modal-heading",
+          "bx-modal-body",
+          "bx-checkbox",
         ].includes(tag),
     },
   },

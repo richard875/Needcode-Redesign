a70286b6660207551330736efcf2f5722e0af47f
Richard Lee <richard_875@me.com>
2022-06-27 22:21:46 +1000

    - Add about model and dynamic progress circle rendering

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -11,6 +11,7 @@ export default {
   data() {
     return {
       currentTab: 0 as Number,
+      openModal: false as Boolean
     };
   },
   methods: {
@@ -18,6 +19,13 @@ export default {
       this.currentTab = index;
       this.$emit("updateCurrentTab", this.currentTab);
     }
+  },
+  computed: {
+    totalNumberOfQuestion() {
+      let total: number = 0;
+      this.questions.forEach((questionSet: QuestionSet) => total += questionSet.questions.length);
+      return total
+    },
   },
 };
 </script>
@@ -33,10 +41,14 @@ export default {
     <!-- Tags -->
     <div class="p-3 select-none">
       <div class="mb-1 ml-2 flex items-center">
-        <SharedProgressCircle />
+        <SharedProgressCircle
+          :percentage="Math.round((10 / totalNumberOfQuestion) * 100)"
+        />
         <bx-tag class="ml-2 text-xs text-gray-900" type="blue">
-          <p>&nbsp;75 / 150 questions completed&nbsp;</p></bx-tag
-        >
+          <p>
+            &nbsp;10 / {{ totalNumberOfQuestion }} questions completed&nbsp;
+          </p>
+        </bx-tag>
       </div>
 
       <bx-tag class="cursor-pointer bg-gray-800 text-white" type="gray">
@@ -93,9 +105,57 @@ export default {
           All rights reserved
         </p>
       </div>
-      <bx-btn class="w-full">About</bx-btn>
+      <bx-btn class="w-full" @click="openModal = true">About</bx-btn>
     </div>
   </div>
+
+  <!-- About modal box -->
+  <bx-modal :open="openModal" @bx-modal-beingclosed="openModal = false">
+    <bx-modal-header>
+      <bx-modal-close-button></bx-modal-close-button>
+      <bx-modal-label>About</bx-modal-label>
+      <bx-modal-heading>
+        <span>
+          <b>TLDR</b>
+          : A <b>better</b> way to prepare for coding interviews
+        </span>
+      </bx-modal-heading>
+    </bx-modal-header>
+    <bx-modal-body class="px-5 mb-5">
+      <div class="leading-6">
+        <p>
+          &#x2022; The <b>Blind 75</b> is a popular list of Algorithm practice
+          problems. I created the <b>Neetcode 150</b> by adding 75 more problems
+          to make a more beginner friendly and more comprehensive list.
+        </p>
+        <p>
+          &#x2022; Try starting with a few Easy problems from each topic, or
+          maybe solve each topic all at once.
+        </p>
+        <p>
+          &#x2022; Get stuck? I've created a <b>thorough</b> &#127909; video
+          explanation for each problem.
+        </p>
+        <p>
+          &#x2022; Some of the problems require LC premium (e.g. Meeting Rooms),
+          in that case I've linked to a free alternative site instead.
+        </p>
+      </div>
+
+      <!-- Divider -->
+      <bx-side-nav-divider class="my-5"></bx-side-nav-divider>
+
+      <div class="video-container">
+        <iframe
+          src="https://www.youtube.com/embed/aa2ijyWBBIc"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </bx-modal-body>
+  </bx-modal>
 </template>
 
 <style lang="scss" scoped>
@@ -121,6 +181,20 @@ export default {
 
   .bx-side-nav-link-active {
     border-left-color: #2e62fe;
+  }
+}
+
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
   }
 }
 </style>

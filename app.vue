4a07ef35140ec6ac167f012604ed1762896157fa
Richard Lee <richard_875@me.com>
2022-06-27 16:10:14 +1000

    - Modify App component

--- app.vue
+++ app.vue
@@ -1,1 +1,121 @@
-<template></template>
+<script>
+import "carbon-web-components/es/components/tooltip/index.js";
+import "carbon-web-components/es/components/search/index.js";
+import "carbon-web-components/es/components/button/index.js";
+import "carbon-web-components/es/components/ui-shell/index.js";
+import "carbon-web-components/es/components/tag/index.js";
+import "carbon-web-components/es/components/data-table/index.js";
+import "carbon-web-components/es/components/notification/index.js";
+import data from "./assets/data/questions.json";
+
+console.log(data);
+</script>
+
+<template>
+  <div>
+    <!-- Top bar -->
+    <NavBar />
+
+    <!-- Left side -->
+    <SideBar />
+
+    <!-- Right side -->
+    <div class="ml-80 p-10">
+      <p class="mb-2 text-xl">Arrays & Hashing</p>
+      <div class="mb-4 flex items-center">
+        <ProgressCircle />
+        <bx-tag class="ml-2 text-xs text-gray-900" type="blue">
+          <p>&nbsp;7 / 9 questions completed&nbsp;</p></bx-tag
+        >
+      </div>
+
+      <bx-data-table>
+        <bx-table>
+          <bx-table-head>
+            <bx-table-header-row>
+              <bx-table-header-cell>Status</bx-table-header-cell>
+              <bx-table-header-cell>Problem</bx-table-header-cell>
+              <bx-table-header-cell>Difficulty</bx-table-header-cell>
+              <bx-table-header-cell>Video Solution</bx-table-header-cell>
+              <bx-table-header-cell>Code</bx-table-header-cell>
+            </bx-table-header-row>
+          </bx-table-head>
+          <bx-table-body>
+            <bx-table-row>
+              <bx-table-cell>Foo1</bx-table-cell>
+              <bx-table-cell>Bar1</bx-table-cell>
+              <bx-table-cell>Baz1</bx-table-cell>
+              <bx-table-cell>Baz1</bx-table-cell>
+              <bx-table-cell>Baz1</bx-table-cell>
+            </bx-table-row>
+            <bx-table-row>
+              <bx-table-cell>Foo2</bx-table-cell>
+              <bx-table-cell>Bar2</bx-table-cell>
+              <bx-table-cell>Baz2</bx-table-cell>
+              <bx-table-cell>Baz2</bx-table-cell>
+              <bx-table-cell>Baz2</bx-table-cell>
+            </bx-table-row>
+            <bx-table-row>
+              <bx-table-cell>Foo3</bx-table-cell>
+              <bx-table-cell>Bar3</bx-table-cell>
+              <bx-table-cell>Baz3</bx-table-cell>
+              <bx-table-cell>Baz3</bx-table-cell>
+              <bx-table-cell>Baz3</bx-table-cell>
+            </bx-table-row>
+          </bx-table-body>
+        </bx-table>
+      </bx-data-table>
+    </div>
+
+    <!-- <div>
+      <span class="IBMPlexMono-Bold">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-BoldItalic">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-ExtraLight">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-ExtraLightItalic"
+        >Lorem ipsum dolor sit amet</span
+      >
+    </div>
+    <div>
+      <span class="IBMPlexMono-Italic">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-Light">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-LightItalic">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-Medium">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-MediumItalic">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-Regular">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-SemiBold">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-SemiBoldItalic">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-Text">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-TextItalic">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-Thin">Lorem ipsum dolor sit amet</span>
+    </div>
+    <div>
+      <span class="IBMPlexMono-ThinItalic">Lorem ipsum dolor sit amet</span>
+    </div> -->
+  </div>
+</template>

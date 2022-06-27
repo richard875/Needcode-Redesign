449794e16f7b6bf81b542ce7769a4eb7c52e1843
Richard Lee <richard_875@me.com>
2022-06-28 08:05:02 +1000

    - Rename FontExample to Typography

---
+++ components/test/Typography.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <span class="IBMPlexMono-Bold">IBMPlexMono-Bold</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-BoldItalic">IBMPlexMono-BoldItalic</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-ExtraLight">IBMPlexMono-ExtraLight</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-ExtraLightItalic"
+      >IBMPlexMono-ExtraLightItalic</span
+    >
+  </div>
+  <div>
+    <span class="IBMPlexMono-Italic">IBMPlexMono-Italic</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-Light">IBMPlexMono-Light</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-LightItalic">IBMPlexMono-LightItalic</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-Medium">IBMPlexMono-Medium</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-MediumItalic">IBMPlexMono-MediumItalic</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-Regular">IBMPlexMono-Regular</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-SemiBold">IBMPlexMono-SemiBold</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-SemiBoldItalic">IBMPlexMono-SemiBoldItalic</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-Text">IBMPlexMono-Text</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-TextItalic">IBMPlexMono-TextItalic</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-Thin">IBMPlexMono-Thin</span>
+  </div>
+  <div>
+    <span class="IBMPlexMono-ThinItalic">IBMPlexMono-ThinItalic</span>
+  </div>
+</template>

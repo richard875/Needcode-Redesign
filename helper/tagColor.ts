55cea9db97e589f31c803f9f1468044abb1d2d58
Richard Lee <richard_875@me.com>
2022-06-28 06:23:46 +1000

    - Add TagColor helper function

---
+++ helper/tagColor.ts
@@ -0,0 +1,16 @@
+import Difficulty from "../src/enum/difficulty";
+
+const TagColor = (difficulty: number) => {
+  switch (difficulty) {
+    case Difficulty.Easy:
+      return "green";
+    case Difficulty.Medium:
+      return "blue";
+    case Difficulty.Hard:
+      return "magenta";
+    default:
+      return "gray";
+  }
+};
+
+export default TagColor;

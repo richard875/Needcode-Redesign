9dd7f904ade1f3e93563a876009c37e7d473c8b3
Richard Lee <richard_875@me.com>
2022-07-01 07:14:38 +1000

    - iOS Disable Auto Zoom in Input "Text" tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,10 @@ export default defineNuxtConfig({
       title: "A better way to prepare for coding interviews",
       titleTemplate: "%s - Neetcode.io",
       meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, maximum-scale=1",
+        },
         // {
         //   hid: "description",
         //   name: "description",

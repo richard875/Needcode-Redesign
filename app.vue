8edf55d2fab8676f9fc825fb9218ec4459549b44
Richard Lee <richard_875@me.com>
2022-07-01 21:52:08 +1000

    - Improve mobile view

--- app.vue
+++ app.vue
@@ -81,7 +81,7 @@ export default {
     />
 
     <!-- Right side -->
-    <div class="ml-0 lg:ml-80 !pt-20 p-5 lg:p-10">
+    <div class="ml-0 lg:ml-80 !pt-16 lg:!pt-20 p-3 lg:p-10">
       <QuestionSection
         :questions="questions"
         :current-tab="currentTab"

0326d8d1d12273126c6c44ebc7de6a3a0ef1d8d4
Richard Lee <richard_875@me.com>
2022-07-03 19:42:28 +1000

    - Test2

--- app.vue
+++ app.vue
@@ -28,6 +28,7 @@ export default {
   },
   mounted() {
     this.displayLoading = false;
+    console.log(GOOGLE_ANALYTICS_ID);
   },
   methods: {
     emitUpdateCurrentTab(index: number) {
@@ -93,7 +94,6 @@ export default {
       @click="menuOpen = false"
       class="ml-0 lg:ml-80 !pt-16 lg:!pt-20 p-3 lg:p-10"
     >
-      <p>{{ process.env.GOOGLE_ANALYTICS_ID }}</p>
       <QuestionSection
         :questions="questions"
         :current-tab="currentTab"

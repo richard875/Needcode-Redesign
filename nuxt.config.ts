d9c4ff96ad2cef34c35c4cdf1d0df3f40baba28c
Richard Lee <richard_875@me.com>
2022-07-03 21:05:27 +1000

    - Add nuxt GA properties

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,6 +34,11 @@ export default defineNuxtConfig({
       ],
     },
   },
+  runtimeConfig: {
+    public: {
+      googleAnalyticsId: "",
+    },
+  },
   typescript: {
     shim: false,
   },
--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,12 +1,8 @@
 import VueGtag from "vue-gtag-next";
-// import { GOOGLE_ANALYTICS_ID } from "../key/googleAnalytics";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const id = process.env.GOOGLE_ANALYTICS_ID;
-  console.log(`Say hello: ${id}`);
-  console.log(process.env);
-  console.log(process.env.NODE_VERSION);
-  console.log(process.env.NODE_ENV);
+  const config = useRuntimeConfig();
+  const id = config.public.googleAnalyticsId;
 
   nuxtApp.vueApp.use(VueGtag, {
     property: {
